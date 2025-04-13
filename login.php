19b4123daeba203d68fcdac7894562a88a9e636d
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-14 00:09:15 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php
-// login.php
-$redirect = urlencode("https://dash.novarix-studio.de/start_oauth.php");
-header("Location: https://discord.com/login?redirect_to=$redirect");
-exit;
+$client_id = '1284484623279067196';
+$redirect_uri = urlencode('https://dash.novarix-studio.de/callback.php');
+$scope = 'identify email guilds';
+$response_type = 'code';
+
+$auth_url = "https://discord.com/oauth2/authorize?client_id=$client_id&response_type=$response_type&redirect_uri=$redirect_uri&scope=$scope";
+header("Location: $auth_url");
+exit();
+?>
