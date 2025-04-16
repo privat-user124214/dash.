366fef7beb4b3b8dee017da8a88fd8f886d6b23f
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-16 03:37:01 +0200

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,12 @@
 $client_id = '1284484623279067196';
 $redirect_uri = urlencode('https://dash.novarix-studio.de/callback.php');
 $scope = 'identify email guilds';
-$response_type = 'code';
 
-$auth_url = "https://discord.com/oauth2/authorize?client_id=$client_id&response_type=$response_type&redirect_uri=$redirect_uri&scope=$scope";
+$oauth_path = "/oauth2/authorize?client_id=$client_id&response_type=code&redirect_uri=$redirect_uri&scope=$scope";
+
+// Leitet zu Discord Login, wenn nicht eingeloggt – sonst direkt zu Oauth2
+$auth_url = "https://discord.com/login?redirect_to=" . urlencode($oauth_path);
+
 header("Location: $auth_url");
-exit();
+exit;
 ?>
