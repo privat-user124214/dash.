403508b333c6bc1d85690e696144410846e659aa
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-17 03:27:13 +0200

    Update db.php

--- db.php
+++ db.php
@@ -2,17 +2,15 @@
 $host = 'ep-nameless-water-a2zd2tqy-pooler.eu-central-1.aws.neon.tech';
 $db   = 'neondb';
 $user = 'neondb_owner';
-$pass = 'npg_I5GjqmM6iKxR';
-$charset = 'utf8';
+$pass = 'npg_I5GjqmM6iKxR'; // dein echtes Passwort
+$sslmode = 'require';
 
-$dsn = "pgsql:host=$host;dbname=$db;sslmode=require";
+$dsn = "pgsql:host=$host;dbname=$db;sslmode=$sslmode";
 
 try {
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
-    // Verbindung erfolgreich
 } catch (PDOException $e) {
-    die('Verbindung fehlgeschlagen: ' . $e->getMessage());
+    die("Fehler bei der DB-Verbindung: " . $e->getMessage());
 }
-?>
