74ecc0dc3d7d9dad603bd62a4ebb22091e158ea3
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-28 01:40:45 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,13 @@
 <?php
+// === Discord App Einstellungen ===
 $client_id = '1284484623279067196';
-$client_secret = 'rWr9kGV33-rTIvhv6ACi-NrJVxbQFQKy'; // Ersetzen!
+$client_secret = 'rWr9kGV33-rTIvhv6ACi-NrJVxbQFQKy'; // Ersetzen, falls nötig
 $redirect_uri = 'https://dash-k7s3.onrender.com/callback.php';
+
+// === Datenbank Zugangsdaten ===
+define('DB_HOST', 'ep-nameless-water-a2zd2tqy.eu-central-1.aws.neon.tech');
+define('DB_PORT', '5432');
+define('DB_NAME', 'neondb');
+define('DB_USER', 'neondb_owner');
+define('DB_PASS', 'npg_I5GjqmM6iKxR');
 ?>
